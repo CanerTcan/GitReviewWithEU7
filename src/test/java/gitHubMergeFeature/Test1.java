a722053a3a715355592c2c0f6e4af842eca674b4
CanerTcan <turkcancaner@gmail.com>
2021-12-19 20:51:14 +0100

    New Trials new commit

---
+++ src/test/java/gitHubMergeFeature/Test1.java
@@ -0,0 +1,9 @@
+package gitHubMergeFeature;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("I made this cahnges locally but I will merge them with the github");
+    }
+}
